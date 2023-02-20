8459ebe52a6ab4cad4c226e9d0343ce1c28bc9b0
lahiruakmeemana <lahiruakmeemana@gmail.com>
2023-02-21 00:20:15 +0530

    Data pull and combine scripts

---
+++ monitoring/data_collection/cadvisor_metrics_per_service.py
@@ -0,0 +1,47 @@
+from influxdb_client import InfluxDBClient
+from influxdb_client.client.write_api import SYNCHRONOUS
+import pandas as pd
+import os
+import time
+token = 'amcAeToLou5FOnvhv8czQYOfD_2fMIx2MjiUyZgpLBoRsXh0_HNQr1-i-hB92OsFeSH6nnhcoDeaVxiaCQLbSw=='
+org = "fyp"
+# create a client to connect to the InfluxDB 2.0 server
+client = InfluxDBClient(url="http://10.8.100.247:8086", token=token, org=org,timeout=300_000)
+now_time = str(int(time.time()))
+# select the bucket to use
+bucket = "robot-shop"
+
+st_time = 1676190960
+end_time = 1676660400 #st_time + 3600 * 24 * 4
+step = 3600
+containers = ['user', 'dispatch', 'ratings', 'payment', 'shipping', 'mongodb', 'web', 'cart', 'redis', 'mysql', 'rabbitmq', 'catalogue']
+for container in containers:
+    print(container)
+    for i,st in enumerate(range(st_time,end_time-step,step)):
+        print(st)
+        # create a query string to select all data from a measurement
+        query = f'from(bucket: "{bucket}") |> range(start: {st}, stop: {st+step}) \
+                |> filter(fn: (r) => r._measurement == "prometheus_remote_write" and r.name == "robot-shop-{container}-1")\
+                |> keep(columns:["_time","_value","_field"])\
+                |> pivot(rowKey:["_time"], columnKey: ["_field"],valueColumn: "_value")'
+
+        print("query: ",query)
+        # create a query API client to execute the query
+        query_api = client.query_api()
+        csv_result = query_api.query_data_frame(org=org,query=query)
+
+        # data = [record.values for table in results for record in table.records]
+        # print('here2')
+        # create a DataFrame from the query results
+        df = pd.DataFrame(csv_result)
+        out_dir = os.path.join(os.getcwd(),"data",now_time,str(i))
+        if not os.path.exists(out_dir):
+            print("making dir")
+            os.makedirs(out_dir)
+
+        df.to_csv(os.path.join(out_dir,container+ ".csv"),index=False)
+        
+
+
+
+
---
+++ monitoring/data_collection/combine_csv_cadvisor.py
@@ -0,0 +1,50 @@
+import glob
+import pandas as pd
+
+folder_path = 'D:/ACA/fyp/bsc-fyp/monitoring/data_collection/data/cadvisor/'
+files = glob.glob(folder_path+'*/*')
+print(len(files))
+total_hours = len(glob.glob(folder_path+'*'))
+print('hours:',total_hours)
+services = ['user', 'dispatch', 'ratings', 'payment', 'shipping', 'mongodb', 'web', 'cart', 'redis', 'mysql', 'rabbitmq', 'catalogue']
+drop_cols = ['container_last_seen','container_spec_cpu_period',	'container_spec_cpu_shares',	'container_spec_memory_limit_bytes',	'container_spec_memory_reservation_limit_bytes',	'container_spec_memory_swap_limit_bytes',	'container_start_time_seconds']
+# Update row number of last available datapoint in last hour
+last_row = 722
+for j,service in enumerate(services):
+  data_service = []
+  service_cols = []
+  for i in range(0,total_hours):
+    if i==(total_hours-1):skiprows=last_row
+    else :skiprows=722
+
+    path = folder_path+str(i)+'/'+service+'.csv'        #change this accordingly
+    data = pd.read_csv(path)
+    dataframe = pd.DataFrame(data)
+    cols = ['_time']
+    cols +=  [col for col in dataframe.columns if (col.startswith('container_') and (col not in drop_cols))] 
+    df = dataframe.loc[:, cols]
+    size = df.shape
+    print(path)
+    print('size: ',size)
+    df['_time'] = pd.to_datetime(df['_time'])
+
+    max_timestamp = df['_time'][0]
+    for i, row in df.iterrows():
+        if row['_time'] < max_timestamp:
+            df = df.iloc[:i]
+            break
+        max_timestamp = row['_time']
+
+    df = df.fillna(method='ffill')
+
+    data_service.extend(df.values.tolist())
+    service_cols = cols
+
+  for i in range(len(service_cols)):
+    if not service_cols[i].startswith('container_'):
+      service_cols[i] = service+'_'+service_cols[i]
+  service_cols.pop(0)
+  service_cols.insert(0,'timestamp')
+  service_df = pd.DataFrame(data_service,columns=service_cols)
+  service_df.to_csv('test/'+service+'.csv')
+
