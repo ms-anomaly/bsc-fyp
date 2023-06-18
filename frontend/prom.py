27bd331d5278c6f4f5af369a32c094b018da05af
Husni Faiz <ahamedhusni73@gmail.com>
2023-06-18 16:52:12 +0530

    use pandas

--- frontend/prom.py
+++ frontend/prom.py
@@ -18,8 +18,7 @@ def getData():
         current_timestamp = datetime.datetime.now().timestamp()
         start_timestamp = datetime.datetime.fromtimestamp(current_timestamp-120).strftime("%Y-%m-%dT%H:%M:%S.%fZ") # endtime - 2 minutes
         end_timestamp = datetime.datetime.fromtimestamp(current_timestamp).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
-        window_5min_timestamp = datetime.datetime.fromtimestamp(current_timestamp-120-300).strftime("%Y-%m-%dT%H:%M:%S.%fZ") # endtime - 2 minutes
-        window_30min_timestamp = datetime.datetime.fromtimestamp(current_timestamp-120-1800).strftime("%Y-%m-%dT%H:%M:%S.%fZ") # endtime - 2 minutes
+        window_30min_timestamp = datetime.datetime.fromtimestamp(current_timestamp-1800).strftime("%Y-%m-%dT%H:%M:%S.%fZ") # endtime - 2 minutes
 
         print(start_timestamp, end_timestamp)
         # Iterate for each service
@@ -135,87 +134,6 @@ def getData():
                 n += 1
 
             if const.rt_metrics[m][0] !=0:
-                # RT Step sum
-                for rt in const.rt_metrics[m]:
-                    q = '{0}'.format(rt)
-                    # DEBUG
-                    # print(q)
-
-                    # Start and end times are inclusive in range query
-                    response = requests.get(PROMETHEUS + PROMETHEUS_ENDPOINT_RANGE_QUERY, 
-                                            params={
-                                                'query': q,
-                                                'start': start_timestamp,
-                                                'end': end_timestamp,
-                                                'step':'5s'
-                                                })
-
-                    results = response.json()
-                    # DEBUG
-                    # print(results)
-                    dataV = results['data']
-                    if len(dataV['result']) < 1:
-                        break
-                    # print(dataV['result'][0]['values'])
-                    periodData = dataV['result'][0]['values']
-
-                    if len(periodData) < 1:
-                        break
-
-
-                # DEBUG: Print RT values of a feature
-                # print(m, n,": ", end="")
-                # for k in range(24):                        
-                #     print(period[k][m][n], end=" ")
-                # DEBUG
-                # print('\n')
-
-                n += 1
-
-                # RT 5min Window sum
-                for rt in const.rt_metrics[m]:
-                    q = '{0}'.format(rt)
-
-                    # Start and end times are inclusive in range query
-                    response = requests.get(PROMETHEUS + PROMETHEUS_ENDPOINT_RANGE_QUERY, 
-                                            params={
-                                                'query': q,
-                                                'start': window_5min_timestamp,
-                                                'end': end_timestamp,
-                                                'step':'5s'
-                                                })
-
-                    results = response.json()
-                    # DEBUG
-                    # print(results)
-                    dataV = results['data']
-                    if len(dataV['result']) < 1:
-                        break
-                    # print(dataV['result'][0]['values'])
-                    periodData = dataV['result'][0]['values']
-
-                    if len(periodData) < 1:
-                        break
-
-
-                    movingAvg5min = 0
-                    if len(periodData)==60:
-                        for i in range(12*5):
-                            movingAvg5min += float(periodData[i][1])
-                            
-                        for k in range(24):
-                            period[k][m][n] += movingAvg5min
-                            movingAvg5min = float(periodData[k+12*5][1]) - float(periodData[k][1])
-
-                # DEBUG: Print RT values of a feature
-                # print(m, n,": ", end="")
-                # for k in range(24):                        
-                #     print(period[k][m][n], end=" ")
-                # DEBUG
-                # print('\n')
-
-                n += 1
-
                 # RT 30min Window sum
                 for rt in const.rt_metrics[m]:
                     q = '{0}'.format(rt)
@@ -237,18 +155,14 @@ def getData():
                         break
                     # print(dataV['result'][0]['values'])
                     periodData = dataV['result'][0]['values']
+                    data30min = [float(i[1]) for i in periodData]
 
                     if len(periodData) < 1:
                         break
-
-                    movingAvg30min = 0
-                    if len(periodData)==60:
-                        for i in range(12*5):
-                            movingAvg30min += float(periodData[i][1])
                             
-                        for k in range(24):
-                            period[k][m][n] += movingAvg30min
-                            movingAvg30min = float(periodData[k+12*5][1]) - float(periodData[k][1])
+                    for k in range(24):
+                        # Assign feature 20,21 and 22 HERE
+                        period[k][m][n] += 0
 
                 # DEBUG: Print RT values of a feature
                 # print(m, n,": ", end="")
